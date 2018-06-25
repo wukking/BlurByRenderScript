63a31d52e8b6282b2d2ed6f492c84fb682d9b830
SunAscend <31230004+SunAscend@users.noreply.github.com>
2018-06-25 15:07:37 +0800

    添加图片压缩BitmapUtils

--- app/src/main/java/com/wuyson/blurbyrenderscript/CacheUtils.java
+++ app/src/main/java/com/wuyson/blurbyrenderscript/CacheUtils.java
@@ -17,9 +17,20 @@ public class CacheUtils {
         mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
             @Override
             protected int sizeOf(String key, Bitmap bitmap) {
-                return bitmap.getRowBytes() * bitmap.getHeight() / 1024;
+//                return bitmap.getRowBytes() * bitmap.getHeight() / 1024;
+                return bitmap.getByteCount()/1024;
             }
         };
+    }
+
+    public void addBitmapToMemoryCache(String key, Bitmap bitmap) {
+        if (getBitmapFromMemCache(key) == null) {
+            mMemoryCache.put(key, bitmap);
+        }
+    }
+
+    public Bitmap getBitmapFromMemCache(String key) {
+        return mMemoryCache.get(key);
     }
 
     public static void diskLru(){
